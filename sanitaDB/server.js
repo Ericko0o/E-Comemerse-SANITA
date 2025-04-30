1861cde3076dea6db0e23ad8409ae22056f46abc
Ericko0o <erick.villacis@ucsp.edu.pe>
2025-04-30 10:49:28 -0500

    Version Erick 2.4 (Optimizacion de DB)

--- sanitaDB/server.js
+++ sanitaDB/server.js
@@ -42,15 +42,8 @@ app.get('/', (req, res) => {
 // Crear o abrir base de datos
 const db = new sqlite3.Database('./sanitadatabase.db');
 
-// Operaciones en serie para evitar conflictos
+// Crear tabla productos si no existe
 db.serialize(() => {
-  // Crear tabla items si no existe
-  db.run(`CREATE TABLE IF NOT EXISTS items (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT
-  )`);
-
-  // Crear tabla productos si no existe
   db.run(`CREATE TABLE IF NOT EXISTS productos (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre TEXT,
@@ -58,58 +51,6 @@ db.serialize(() => {
     imagen TEXT,
     categoria TEXT
   )`);
-
-  // Insertar productos si la tabla 'productos' está vacía
-  db.get("SELECT COUNT(*) as count FROM productos", (err, row) => {
-    if (err) {
-      console.error('Error verificando productos:', err.message);
-      return;
-    }
-
-    if (row.count === 0) {
-      const stmt = db.prepare("INSERT INTO productos (nombre, precio, imagen, categoria) VALUES (?, ?, ?, ?)");
-
-      // CICATRIZANTES
-      stmt.run("Sangre de Grado", 20, "IMG/sangre-de-grado.jpg", "cicatrizantes");
-      stmt.run("Matico", 15, "IMG/matico.jpeg", "cicatrizantes");
-      stmt.run("Llanten", 25, "IMG/llanten.jpg", "cicatrizantes");
-      stmt.run("Flor de Arena", 25, "IMG/flor-de-arena.jpg", "cicatrizantes");
-      stmt.run("Chilca", 20, "IMG/chilca.jpg", "cicatrizantes");
-
-      // INMUNOLÓGICO
-      stmt.run("Uña de Gato", 20, "IMG/una-de-gato.jpg", "inmunologico");
-      stmt.run("Ayahuasca", 15, "IMG/ayahuasca.jpeg", "inmunologico");
-      stmt.run("Sacha Inchi", 25, "IMG/sacha-inchi.jpg", "inmunologico");
-      stmt.run("Muña", 25, "IMG/muna.jpg", "inmunologico");
-      stmt.run("Chuchuhuasi", 20, "IMG/chuchuhuasi.jpg", "inmunologico");
-
-      stmt.finalize();
-      console.log('Productos insertados automáticamente.');
-    } else {
-      console.log('Productos ya existentes, no se insertaron de nuevo.');
-    }
-  });
-});
-
-// Endpoint para obtener todos los items
-app.get('/items', (req, res) => {
-  db.all("SELECT * FROM items", (err, rows) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.json({ items: rows });
-  });
-});
-
-// Endpoint para agregar nuevo item
-app.post('/add', (req, res) => {
-  const { name } = req.body;
-  db.run("INSERT INTO items (name) VALUES (?)", [name], function(err) {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.json({ message: 'Item agregado', id: this.lastID });
-  });
 });
 
 // Endpoint para obtener todos los productos
