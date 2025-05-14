2a79a08caa8e20c327e1d056ebc16a2cd897e28b
unknown <shantall.sulla@ucsp.edu.pe>
2025-05-13 19:46:50 -0500

    Version Shantall 4.4

--- JS/pago.js
+++ JS/pago.js
@@ -1,3 +1,49 @@
+const campoFecha = document.getElementById("fecha-exp");
+
+const campoTarjeta = document.getElementById("numero-tarjeta");
+
+campoTarjeta.addEventListener("input", function (e) {
+  let valor = e.target.value.replace(/\D/g, ""); // Solo dígitos
+
+  // Limita a 18 dígitos
+  if (valor.length > 18) {
+    valor = valor.slice(0, 18);
+  }
+
+  // Inserta un espacio cada 4 dígitos
+  const formateado = valor.match(/.{1,4}/g)?.join(" ") ?? "";
+
+  e.target.value = formateado;
+
+  // Validación personalizada
+  if (valor.length < 13) {
+    e.target.setCustomValidity("El número debe tener al menos 13 dígitos");
+  } else {
+    e.target.setCustomValidity("");
+  }
+});
+
+campoFecha.addEventListener("input", function (e) {
+  let input = e.target.value.replace(/[^\d]/g, ""); // quitar caracteres no numéricos
+
+  if (input.length >= 3) {
+    const mes = input.substring(0, 2);
+    const anio = input.substring(2, 4);
+
+    // Validar si el mes es mayor a 12
+    if (parseInt(mes) > 12) {
+      e.target.setCustomValidity("El mes debe estar entre 01 y 12");
+    } else {
+      e.target.setCustomValidity("");
+    }
+
+    e.target.value = mes + "/" + anio;
+  } else {
+    e.target.value = input;
+    e.target.setCustomValidity("");
+  }
+});
+
 document.getElementById("pago-form").addEventListener("submit", async (e) => {
   e.preventDefault();
   document.getElementById("mensaje-pago").textContent = "Procesando pago...";
@@ -9,7 +55,7 @@ document.getElementById("pago-form").addEventListener("submit", async (e) => {
     document.getElementById("mensaje-pago").innerHTML = `
       <h2>¡Gracias por tu compra!</h2>
       <p>Tu pedido ha sido registrado correctamente.</p>
-      <a href="/catalogo.html">Volver al catálogo</a>
+      <a href="/catalogo.html" class="volver-catalogo">Volver al catálogo</a>
     `;
   }, 2000);
 });
